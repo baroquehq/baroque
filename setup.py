d559ed5bb3403afad9dae0bc4274fbdc97663a37
csparpa <csparpa@gmail.com>
2017-04-04 21:49:43 +0200

    This really fixes #1

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import os
 from setuptools import find_packages, setup
-from baroque.constants import BAROQUE_VERSION
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='baroque',
-    version='.'.join(map(str, BAROQUE_VERSION)),
+    version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[

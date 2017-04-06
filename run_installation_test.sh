f15f3e17aa2dc9234d289cfc1aa9225166a9a410
csparpa <csparpa@gmail.com>
2017-04-06 19:17:52 +0200

    Installation test

---
+++ run_installation_test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo " --- Start of installation test ..."
+venv="$(date +%s)_brq_install_test"
+virtualenv "$venv" && cd "$venv" && source bin/activate
+echo " --- Created virtualenv: $venv ..."
+pip install baroque
+if [ $? -ne 0 ]; then
+    echo " --- Installation through pip failed!"
+    exit 1
+fi
+echo " --- Installation through pip was OK ..."
+python3 -c "import baroque"
+if [ $? -ne 0 ]; then
+    echo " --- Test import of library failed!"
+    exit 2
+fi
+echo " --- Test import of library was OK ..."
+deactivate
+echo " --- End of installation test"
+exit 0

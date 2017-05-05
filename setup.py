02c1fa1ea94eb2128ac781d45e54328f7e40e3d3
csparpa <csparpa@gmail.com>
2017-05-05 11:52:33 +0200

    Dump to version 0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='baroque',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
